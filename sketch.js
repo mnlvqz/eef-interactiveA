afba42d56b3e939f7cedd244ea5b7e01d3c9bde9
mnlvqz <44558123+mnlvqz@users.noreply.github.com>
2023-11-16 11:57:12 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
+let utilityPole;
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   frameRate(60);
   imageMode(CENTER);
+
+  utilityPole = new Pole(createVector(0, 0, 0), 1024);
 }
 
 function draw() {
   background(128);
+  orbitControl();
+  utilityPole.drawPole();
 }
 
 function windowResized() {
