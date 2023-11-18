753b04c4ddaa98632fbbb9bbeab53c2e187b6038
mnlvqz <44558123+mnlvqz@users.noreply.github.com>
2023-11-18 13:50:25 -0600

    Improve insulator generation

--- objects/UtilityPole.js
+++ objects/UtilityPole.js
@@ -18,8 +18,9 @@ class UtilityPole {
     pop();
 
     for (let i = 0; i < this.crossArms.length; i++) {
-      push();
       // Cross-arms
+      push();
+      //Cross-arm
       rotateY(this.crossArms[i].angle);
       translate(
         this.crossArms[i].position.x,
@@ -31,28 +32,22 @@ class UtilityPole {
         this.crossArms[i].dimensions.y,
         this.crossArms[i].dimensions.z
       );
-
-      // Insulators
-      translate(
-        this.crossArms[i].dimensions.x * 0.5 - this.crossArms[i].insulator,
-        -this.crossArms[i].dimensions.y,
-        0
-      );
-      cylinder(this.crossArms[i].insulator, 16);
-      translate(
-        this.crossArms[i].dimensions.x * -0.5 + this.crossArms[i].insulator,
-        0,
-        0
-      );
-      cylinder(this.crossArms[i].insulator, 16);
-      translate(
-        this.crossArms[i].dimensions.x * -0.5 + this.crossArms[i].insulator,
-        0,
-        0
-      );
-      cylinder(this.crossArms[i].insulator, 16);
-
       pop();
+
+      for (let j = 0; j < this.crossArms[i].insulators.length; j++) {
+        let insulator = this.crossArms[i].insulators[j];
+
+        push();
+
+        rotateY(this.crossArms[i].angle);
+        translate(
+          insulator.position.x,
+          insulator.position.y,
+          insulator.position.z
+        );
+        cylinder(insulator.dimensions[0], insulator.dimensions[1]);
+        pop();
+      }
     }
 
     pop();
@@ -62,23 +57,56 @@ class UtilityPole {
     let crossArms = [];
 
     for (let i = 0; i < crossArmsNumber; i++) {
-      let crossArmDimension = createVector(256, 16, 8);
-      let crossArmXOffset = crossArmDimension.x * random(0.1, 0.2);
+      let crossArmDimensions = createVector(256, 16, 8);
+
+      //let crossArmXOffset = crossArmDimensions.x * random(0.1, 0.2);
+      let crossArmXOffset = 0;
+
       let crossArmHeight =
         this.poleHeight * -0.5 * map(i, 0, crossArmsNumber - 1, 0.6, 0.9);
+
+      let crossArmPosition = createVector(
+        crossArmXOffset,
+        crossArmHeight,
+        this.poleRadius * 0.5 + crossArmDimensions.z * 1.5
+      );
+
       crossArms[i] = {
-        dimensions: crossArmDimension,
-        position: createVector(
-          crossArmXOffset,
-          crossArmHeight,
-          this.poleRadius * 0.5 + crossArmDimension.z * 1.5
-        ),
+        dimensions: crossArmDimensions,
+        position: crossArmPosition,
         angle: random(TWO_PI),
-        insulatorAPosition: 0,
-        insulator: 8,
+        insulators: this.generateInsulators(
+          3,
+          crossArmDimensions,
+          crossArmPosition
+        ),
       };
     }
     return crossArms;
+  }
+
+  generateInsulators(insulatorsNumber, crossArmDimensions, crossArmPosition) {
+    let insulators = [];
+    let insulatorDimensions = [8, 16];
+
+    let crossArmFixedLength = crossArmDimensions.x - insulatorDimensions[0] * 2;
+    let insulatorSeparation = crossArmFixedLength / (insulatorsNumber - 1);
+
+    let insulatorFixedHeight = crossArmPosition.y - crossArmDimensions.y;
+
+    for (let i = 0; i < insulatorsNumber; i++) {
+      let insulatorPosition = createVector(
+        crossArmFixedLength * -0.5 + insulatorSeparation * i,
+        insulatorFixedHeight,
+        crossArmPosition.z
+      );
+
+      insulators[i] = {
+        dimensions: insulatorDimensions,
+        position: insulatorPosition,
+      };
+    }
+    return insulators;
   }
 
   generateRandomVectorsFixedRadius(radius, vectorsNumber) {
