242027c3b33df66ed68e31f6f8e9113af3a15663
mnlvqz <44558123+mnlvqz@users.noreply.github.com>
2023-12-15 23:30:31 -0600

    Add comments

--- objects/UtilityPole.js
+++ objects/UtilityPole.js
@@ -4,29 +4,30 @@ class UtilityPole {
     this.innerRadius = innerRadius;
     this.poleRadius = 16;
     this.poleHeight = 1024;
-    this.crossArms = this.generateCrossArms(6);
+    this.crossArms = this.generateCrossArms(20);
   }
 
   updatePole() {}
 
+  // Drawing utility pole
   drawPole() {
+    // Drawing pole
     push();
-
-    // Pole
     push();
     cylinder(this.poleRadius, this.poleHeight);
     pop();
 
+    // Drawing crossarms
     for (let i = 0; i < this.crossArms.length; i++) {
-      // Cross-arms
+      // Drawing each crossarm
       push();
-      //Cross-arm
       rotateY(this.crossArms[i].angle);
       translate(
         this.crossArms[i].position.x,
         this.crossArms[i].position.y,
         this.crossArms[i].position.z
       );
+      //rotateZ(0.1);
       box(
         this.crossArms[i].dimensions.x,
         this.crossArms[i].dimensions.y,
@@ -34,11 +35,11 @@ class UtilityPole {
       );
       pop();
 
+      // Drawing insulators
       for (let j = 0; j < this.crossArms[i].insulators.length; j++) {
+        // Drawing each insulator
         let insulator = this.crossArms[i].insulators[j];
-
         push();
-
         rotateY(this.crossArms[i].angle);
         translate(
           insulator.position.x,
@@ -53,24 +54,31 @@ class UtilityPole {
     pop();
   }
 
+  // Crossarms' generator function
   generateCrossArms(crossArmsNumber) {
+    // Crossarms' array definition
     let crossArms = [];
 
     for (let i = 0; i < crossArmsNumber; i++) {
+      // Crossarm arbitrary dimensions
       let crossArmDimensions = createVector(256, 16, 8);
 
       //let crossArmXOffset = crossArmDimensions.x * random(0.1, 0.2);
+      // Crossarm x-offset
       let crossArmXOffset = 0;
 
+      // Crossarm unitary distribution on y-axis
       let crossArmHeight =
         this.poleHeight * -0.5 * map(i, 0, crossArmsNumber - 1, 0.6, 0.9);
 
+      // Crossarm vector position with z-offset (pole's diameter)
       let crossArmPosition = createVector(
         crossArmXOffset,
         crossArmHeight,
         this.poleRadius * 0.5 + crossArmDimensions.z * 1.5
       );
 
+      // Object literal for crossarm defintion with insulators
       crossArms[i] = {
         dimensions: crossArmDimensions,
         position: crossArmPosition,
@@ -85,15 +93,23 @@ class UtilityPole {
     return crossArms;
   }
 
+  // Insulators' generator function
   generateInsulators(insulatorsNumber, crossArmDimensions, crossArmPosition) {
+    // Insulators' array definition
     let insulators = [];
+
+    // Insulators arbitrary dimensions
     let insulatorDimensions = [8, 16];
 
+    // Crossarms' fixed length, subtract lateral insulators' diameter
     let crossArmFixedLength = crossArmDimensions.x - insulatorDimensions[0] * 2;
+    // Insulators' even separation distance
     let insulatorSeparation = crossArmFixedLength / (insulatorsNumber - 1);
 
+    // Insulators' y-offset, insulator height
     let insulatorFixedHeight = crossArmPosition.y - crossArmDimensions.y;
 
+    // Insulators' vector position array definition
     for (let i = 0; i < insulatorsNumber; i++) {
       let insulatorPosition = createVector(
         crossArmFixedLength * -0.5 + insulatorSeparation * i,
@@ -101,6 +117,7 @@ class UtilityPole {
         crossArmPosition.z
       );
 
+      // Object literal for insulator defintion
       insulators[i] = {
         dimensions: insulatorDimensions,
         position: insulatorPosition,
